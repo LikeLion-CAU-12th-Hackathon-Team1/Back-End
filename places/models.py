f3c916c66564ae3e50096cbdf60f0bb39215c3d7
Haemulramen <apple11078@gmail.com>
2024-07-26 01:37:00 +0900

    FEAT: 시도, 시군구, 장소 모델 생성

---
+++ places/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Place_category(models.IntegerChoices):
+    CAFE = 1, '카페'
+    OFFICE = 2, '공유오피스'
+    NATURE = 3, '자연'
+    FOOD = 4, '먹거리'
+    CULTURE = 5, '문화체험'
+
+class Sido(models.Model):
+    sido_id = models.AutoField(primary_key=True)
+    sido_name = models.CharField(max_length=10, null=False)
+    sido_code = models.SmallIntegerField(null=False)
+
+class Sigg(models.Model):
+    sigg_id = models.AutoField(primary_key=True)
+    sigg_name = models.CharField(max_length=10, null=False)
+    sigg_code = models.SmallIntegerField(null=False)
+
+class Place(models.Model):
+
+    place_id = models.AutoField(primary_key=True)
+    sigg_id = models.ForeignKey(Sigg, on_delete=models.CASCADE)
+    placename = models.CharField(max_length=30, null=False)
+    address = models.CharField(max_length=200, null=False)
+    image = models.CharField(max_length=512, null=True)
+    category = models.IntegerField(choices=Place_category.choices, null=False)
