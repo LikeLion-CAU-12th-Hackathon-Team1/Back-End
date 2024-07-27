ccfb2795f91723a4e788250266f6ebf28cabf214
Haemulramen <apple11078@gmail.com>
2024-07-27 15:49:20 +0900

    FEAT: Place 등록 api 구현 완료

--- places/models.py
+++ places/models.py
@@ -14,14 +14,16 @@ class Sido(models.Model):
 
 class Sigg(models.Model):
     sigg_id = models.AutoField(primary_key=True)
+    sido_id = models.ForeignKey(Sido, on_delete=models.CASCADE)
     sigg_name = models.CharField(max_length=10, null=False)
     sigg_code = models.SmallIntegerField(null=False)
 
 class Place(models.Model):
     place_id = models.AutoField(primary_key=True)
     sigg_id = models.ForeignKey(Sigg, on_delete=models.CASCADE)
+    place_code = models.IntegerField(null=True)
     placename = models.CharField(max_length=30, null=False)
     address = models.CharField(max_length=200, null=False)
-    image = models.CharField(max_length=512, null=True)
+    image = models.URLField(max_length=512, null=True)
     category = models.IntegerField(choices=Place_category.choices, null=False)
     report = models.CharField(max_length=200, null=True)
---
+++ places/request_serializers.py
@@ -0,0 +1,51 @@
+from .models import Place
+from rest_framework import serializers
+import boto3
+from uuid import uuid4
+from django.core.files.base import ContentFile
+from .models import Sigg
+from config.settings import AWS_STORAGE_BUCKET_NAME, AWS_S3_CUSTOM_DOMAIN, AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
+class PlaceCreateSerializer(serializers.ModelSerializer):
+    sigg_id_id = serializers.IntegerField(required=True)
+    placename = serializers.CharField(required=True)
+    address = serializers.CharField(required=True)
+    category = serializers.IntegerField(required=True)
+    photo = serializers.ImageField(write_only=True)
+
+    class Meta:
+        model = Place
+        exclude = ['sigg_id']
+
+    def create(self, validated_data):
+        photo = validated_data.pop('photo', None)
+        photo_url = self.upload_image(photo)
+        validated_data['image'] = photo_url
+        validated_data['sigg_id'] = Sigg.objects.get(pk=validated_data['sigg_id_id'])
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        photo = validated_data.pop('photo', None)
+        if photo:
+            photo_url = self.upload_to_s3(photo)
+            instance.image = photo_url
+
+        instance.place_code = validated_data.get('place_code', instance.place_code)
+        instance.save()
+        return instance
+
+    def upload_image(self, photo):
+        s3_client = boto3.client(
+            's3',
+            region_name=AWS_REGION,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        file_content = ContentFile(photo.read())
+        file_name = f'{uuid4()}.jpg'
+        s3_client.upload_fileobj(
+            file_content,
+            AWS_STORAGE_BUCKET_NAME,
+            f'media/{file_name}',
+            ExtraArgs={'ContentType': 'image/jpeg'}
+        )
+        return f'https://{AWS_S3_CUSTOM_DOMAIN}/media/{file_name}'
