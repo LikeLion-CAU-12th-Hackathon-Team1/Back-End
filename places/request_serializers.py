2d12b25cd8ade3e18e6cc09db893d411c72ca6cf
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-07-30 00:55:00 +0900

    FEAT: resize image 기능 추가

--- places/request_serializers.py
+++ places/request_serializers.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 from django.core.files.base import ContentFile
 from .models import Sigg
 from PIL import Image
+from io import BytesIO
 from config.settings import AWS_STORAGE_BUCKET_NAME, AWS_S3_CUSTOM_DOMAIN, AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 class PlaceCreateSerializer(serializers.ModelSerializer):
@@ -16,7 +17,8 @@ class PlaceCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Place
-        exclude = ['sigg_id']
+        read_only_fields = ('sigg_id',)
+        fields = '__all__'
 
     def create(self, validated_data):
         photo = validated_data.pop('photo', None)
@@ -26,7 +28,7 @@ class PlaceCreateSerializer(serializers.ModelSerializer):
         return super().create(validated_data)
     
     def update(self, instance, validated_data):
-        photo = validated_data.pop('photo', None)
+        photo = validated_data.get('photo')
         if photo:
             photo_url = self.upload_to_s3(photo)
             instance.image = photo_url
@@ -42,9 +44,14 @@ class PlaceCreateSerializer(serializers.ModelSerializer):
             aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
         img = Image.open(photo)
-        width, height = 317, 265
+        width, height = 292, 260
         resized_img = img.resize((width, height))
-        file_content = ContentFile(resized_img.read())
+        img_byte_arr = BytesIO()
+        resized_img.save(img_byte_arr, format='JPEG')
+        img_byte_arr.seek(0)
+        
+        # BytesIO 객체를 ContentFile로 변환
+        file_content = ContentFile(img_byte_arr.read())
         file_name = f'{uuid4()}.jpg'
         s3_client.upload_fileobj(
             file_content,
