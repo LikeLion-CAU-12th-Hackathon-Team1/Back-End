0a6b93474ab53b9149705662b099118a226522fc
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-07-29 20:59:23 +0900

    CHORE: workvalley cors 허용

--- config/settings.py
+++ config/settings.py
@@ -200,6 +200,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'https://likelion-iiovesport.netlify.app',
     'https://saengchaein.r-e.kr',
+    'https://workvalley.netlify.app',
 ]
 
 REST_FRAMEWORK = {
