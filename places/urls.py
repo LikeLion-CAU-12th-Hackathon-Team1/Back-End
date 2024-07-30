f929a018d5225870d3fa8f69f80c323d89102eb1
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-07-30 22:04:26 +0900

    REFACTOR: work, rest 장소 추천 api 쪼개기

--- places/urls.py
+++ places/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', ListCreatePlace.as_view(), name='place-list'),
-    path('<int:sigg_id>/', CategoryPlace.as_view(), name='category-places'),
+    path('work/<int:sigg_id>/', CategoryPlace.as_view(), name='category-places'),
+    path('rest/<int:sigg_id>/', CategoryPlace.as_view(), name='category-places'),
 ]
