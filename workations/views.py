c94529199b6e89e166008974442ae46e8d0e94d2
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-08-04 16:27:49 +0900

    FEAT: 가장 가까운 워케이션에 연결된 데일리 워케이션 id 목록 추가

--- workations/views.py
+++ workations/views.py
@@ -193,6 +193,12 @@ class ClosestFutureWorkation(generics.RetrieveAPIView):
         closest_workation = workations.last()
         if closest_workation:
             serializer = self.get_serializer(closest_workation)
+            data = serializer.data
+
+            daily_workations = Daily_workation.objects.filter(workation=closest_workation.workation_id)
+            daily_workation_list = DailyWorkationSerializer(daily_workations, many=True).data
+            daily_workation_ids = [{'daily_workation_id' : item['daily_workation_id']} for item in daily_workation_list]
+            data['daily_workation_list'] = daily_workation_ids
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response({'detail' : 'No future workations found'}, status=status.HTTP_404_NOT_FOUND)
 
